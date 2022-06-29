fc673988ba92313ee119cab14cafecaef185e187
Alessandro Solbiati <lessandro@fb.com>
2022-06-29 14:02:44 +0100

    [gexf] calling new backend

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     contentBase: path.resolve(__dirname, "./"),
     publicPath: "/dist/",
     host: "localhost",
-    port: 8080,
+    port: 3000,
     open: false,
   },
   resolve: {
