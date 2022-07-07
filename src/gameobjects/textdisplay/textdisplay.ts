8bfe7f02e1cc205d79acda26213ea22843bac8c6
Alessandro Solbiati <lessandro@fb.com>
2022-07-08 00:28:32 +0100

    [textdisplay][1/n] showing label

---
+++ src/gameobjects/textdisplay/textdisplay.ts
@@ -0,0 +1,50 @@
+import { Edge } from "../../graph/graphobjects/edge";
+import { Point } from "../../geometry/point";
+
+import { Constants } from "../../constants";
+import { Events } from "../../events";
+
+import { NodeGeometries } from "../../builders/node-builder";
+
+import { GameObjectOnGraph } from "../../interfaces/graph.interface";
+
+import { MainScene } from "../../scenes/main-scene";
+
+import { GameObject } from "../../gameobjects/gameobject";
+
+class GameObjectWithTextDisplayTypes extends GameObject {
+  get pointCenter(): Point {
+    //@ts-ignore
+    return this.graphParentElement.geometries[NodeGeometries.POINT__CENTER];
+  }
+
+  get graphParentEdge(): Edge {
+    // @ts-ignore
+    return this.graphParentElement;
+  }
+}
+
+export class TextDisplay
+  extends GameObjectWithTextDisplayTypes
+  implements GameObjectOnGraph
+{
+  valid: boolean = true;
+
+  depth: number = 5;
+
+  pointerdown() {}
+
+	constructor(
+		public scene: MainScene,
+		public title: string,
+	){
+		super(scene);
+	}
+
+  populate() {
+		const text = new Phaser.GameObjects.Text(this.scene, this.pointCenter.x, this.pointCenter.y, this.title, null);
+		this.add(text, true);
+    this.setDepth(this.depth);
+    this.setVisible(true);
+  }
+}
